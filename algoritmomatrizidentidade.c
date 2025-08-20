b22c7c977e8dea15c4311f7dbbdb14bf3c5e8240
Gabriel Milagres <ghamilagres@gmail.com>
2025-08-20 11:57:03 -0300

    Atualização: Modularizando o código em funções

--- algoritmomatrizidentidade.c
+++ algoritmomatrizidentidade.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double** algoritmoMatrizI(double** matriz, int tamanho){
-    if(matriz == NULL || tamanho <= 0){
+void verificarMatriz(double** matrizVerificar, int tamanho){
+    if(matrizVerificar == NULL || tamanho <= 0){
         printf("Matriz invalida ou tamanho invalido.\n");
-        return NULL;
     }
+}
+void liberarMatriz(double** matrizLiberar, int tamanho){
+    verificarMatriz(matrizLiberar,tamanho);
+    for(int i = 0; i < tamanho; i++){
+        free(matrizLiberar[i]);
+    }
+    free(matrizLiberar);
+}
+void imprimirMatriz(double** matrizImprimir, int tamanho){
+    verificarMatriz(matrizImprimir,tamanho);
+    for(int i = 0; i < tamanho; i++){
+        for(int j = 0; j < tamanho; j++){
+            printf("%.2lf ", matrizImprimir[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+double** trocarLinhas(double** matrizTrocar, int tamanho){
+    verificarMatriz(matrizTrocar,tamanho);
     double** matrizAuxiliar;
     int contador=0;
-    double valorOposto;
 
     matrizAuxiliar = (double**)malloc(tamanho * sizeof(double*));
     for(int i = 0; i < tamanho; i++){
@@ -21,13 +39,13 @@ double** algoritmoMatrizI(double** matriz, int tamanho){
     }
 
     for(int w = 0; w<tamanho ;w++){
-        if(matriz[w][w] == 0){
+        if(matrizTrocar[w][w] == 0){
             for(int j = w+1 ; j<tamanho;j++){
-                if(matriz[j][w] != 0){
+                if(matrizTrocar[j][w] != 0){
                     for(int z = w ; z<tamanho ; z++){
-                        matrizAuxiliar[0][z] = matriz[j][z];
-                        matriz[j][z] = matriz[w][z];
-                        matriz[w][z] = matrizAuxiliar[0][z];
+                        matrizAuxiliar[0][z] = matrizTrocar[j][z];
+                        matrizTrocar[j][z] = matrizTrocar[w][z];
+                        matrizTrocar[w][z] = matrizAuxiliar[0][z];
                         matrizAuxiliar[0][z] = 0.0;
                     }
                     break;
@@ -41,15 +59,29 @@ double** algoritmoMatrizI(double** matriz, int tamanho){
             }
         }
     }
+    liberarMatriz(matrizAuxiliar,tamanho);
+    return matrizTrocar;
+}
 
+double** operacoesLinhas(double** matrizOperacoes, int tamanho){
+    verificarMatriz(matrizOperacoes,tamanho);
+    double valorOposto;
     for (int z = 0; z<tamanho-1; z++){
         for (int j = z+1; j<tamanho; j++){
-            valorOposto = -matriz[j][z]/matriz[z][z];
+            valorOposto = -matrizOperacoes[j][z]/matrizOperacoes[z][z];
             for (int l = 0; l<tamanho; l++){
-                matriz[j][l] = matriz[z][l]*valorOposto + matriz[j][l];
+                matrizOperacoes[j][l] = matrizOperacoes[z][l]*valorOposto + matrizOperacoes[j][l];
             }
         }
     }
+    return matrizOperacoes;
+}
+
+double** algoritmoMatrizIInferior(double** matriz, int tamanho){
+    verificarMatriz(matriz,tamanho);
+    matriz = trocarLinhas(matriz,tamanho);
+    matriz = operacoesLinhas(matriz,tamanho);
+
     return matriz;
 }
 
@@ -59,12 +91,10 @@ int main(){
 
     printf("Digite o numero de linhas e colunas da matriz quadrada: ");
     scanf("%d", &tamanho);
-
     matriz = (double**)malloc(tamanho * sizeof(double*));
     for(int i = 0; i < tamanho; i++){
         matriz[i] = (double*)malloc(tamanho * sizeof(double));
     }
-
     for(int i = 0; i < tamanho; i++){
         for(int j = 0; j < tamanho; j++){
             printf("Elemento [%d][%d]: ", i, j);
@@ -72,21 +102,10 @@ int main(){
         }
     }
 
-    printf("Matriz escolhida:\n");
-    for(int i = 0; i < tamanho; i++){
-        for(int j = 0; j < tamanho; j++){
-            printf("%.2lf ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-
-    matriz = algoritmoMatrizI(matriz, tamanho);
-
-    printf("Matriz resultante:\n");
-    for(int i = 0; i < tamanho; i++){
-        for(int j = 0; j < tamanho; j++){
-            printf("%.2lf ", matriz[i][j]);
-        }
-        printf("\n");
-    }
+    printf("Matriz Escolhida:\n");
+    imprimirMatriz(matriz, tamanho);
+    matriz = algoritmoMatrizIInferior(matriz, tamanho);
+    printf("Matriz Resultante:\n");
+    imprimirMatriz(matriz, tamanho);
+    liberarMatriz(matriz,tamanho);
 }
