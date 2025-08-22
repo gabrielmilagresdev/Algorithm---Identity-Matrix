cc5b9f0c40292995ad22d965d5d7c3ac663c8fdb
Gabriel Milagres <ghamilagres@gmail.com>
2025-08-22 11:30:43 -0300

    Correção: Bugs e Melhorias

--- algoritmomatrizidentidade.c
+++ algoritmomatrizidentidade.c
@@ -35,54 +35,55 @@ void imprimirMatriz(double** matrizImprimir, int tamanho){
     }
     printf("\n");
 }
-double** trocarLinhas(double** matrizTrocar, int tamanho){
-    verificarMatriz(matrizTrocar,tamanho);
-    double** matrizAuxiliar;
-    int contador=0;
+double** trocarLinhas(double** matrizTrocar, int tamanho) {
+    verificarMatriz(matrizTrocar, tamanho);
+    double* vetorAuxiliar = (double*)malloc(tamanho * sizeof(double));
+    int contador = 0;
 
-    matrizAuxiliar = (double**)malloc(tamanho * sizeof(double*));
-    for(int i = 0; i < tamanho; i++){
-        matrizAuxiliar[i] = (double*)malloc(tamanho * sizeof(double));
-    }
-
-    for(int i = 0; i < tamanho; i++){
-        for(int j = 0; j < tamanho; j++)
-            matrizAuxiliar[i][j] = 0.0;
-    }
-
-    for(int w = 0; w<tamanho ;w++){
-        if(matrizTrocar[w][w] == 0){
-            for(int j = w+1 ; j<tamanho;j++){
-                if(matrizTrocar[j][w] != 0){
-                    if(verificarTroca(matrizTrocar, j)!=NULL){
-                        for(int z = w ; z<tamanho ; z++){
-                            matrizAuxiliar[0][z] = matrizTrocar[j][z];
+    for (int w = 0; w < tamanho; w++) {
+        if (matrizTrocar[w][w] == 0) {
+            contador = 0;
+            for (int j = w+1; j < tamanho; j++) {
+                if (matrizTrocar[j][w] != 0) {
+                    if (verificarTroca(matrizTrocar, j) != NULL) {
+                        for (int z = w; z < tamanho; z++) {
+                            vetorAuxiliar[z] = matrizTrocar[j][z];
                             matrizTrocar[j][z] = matrizTrocar[w][z];
-                            matrizTrocar[w][z] = matrizAuxiliar[0][z];
-                            matrizAuxiliar[0][z] = 0.0;
+                            matrizTrocar[w][z] = vetorAuxiliar[z];
                         }
                         break;
-                    }else{
+                    } else {
                         contador++;
                     }
+                } else {
+                    contador++;
                 }
-         }
-            if(contador == (tamanho-1)){
+            }
+            if (contador == (tamanho - w - 1)) {
                 printf("A MATRIZ NAO E INVERSIVEL!\n");
+                free(vetorAuxiliar);
                 return NULL;
             }
         }
     }
-    liberarMatriz(matrizAuxiliar,tamanho);
+
+    free(vetorAuxiliar);
     return matrizTrocar;
 }
+
+
 
 double** operacoesLinhas(double** matrizOperacoes, int tamanho){
     verificarMatriz(matrizOperacoes,tamanho);
     double valorOposto;
     for (int z = 0; z<tamanho-1; z++){
+        if(matrizOperacoes[z][z] == 0 )
+                matrizOperacoes = trocarLinhas(matrizOperacoes,tamanho);
+        if (matrizOperacoes[z][z] == 0) {
+            printf("Matriz nao inversivel\n");
+            return NULL;
+        }
         for (int j = z+1; j<tamanho; j++){
-            matrizOperacoes = trocarLinhas(matrizOperacoes,tamanho);
             valorOposto = -matrizOperacoes[j][z]/matrizOperacoes[z][z];
             for (int l = 0; l<tamanho; l++){
                 matrizOperacoes[j][l] = matrizOperacoes[z][l]*valorOposto + matrizOperacoes[j][l];
@@ -120,6 +121,8 @@ int main(){
     printf("Matriz Escolhida:\n");
     imprimirMatriz(matriz, tamanho);
     matriz = algoritmoMatrizIInferior(matriz, tamanho);
+    if(matriz == NULL)
+        printf("Matriz nao inversivel");
     printf("Matriz Resultante:\n");
     imprimirMatriz(matriz, tamanho);
     liberarMatriz(matriz,tamanho);
